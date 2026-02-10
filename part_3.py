c51f93a4a476d63c07daa519e54052f20cc94f46
eman20006 <ef392@kent.ac.uk>
2026-02-10 13:49:34 +0000

    unfinished task 3, got up to 3.2

--- part_3.py
+++ part_3.py
@@ -1,1 +1,25 @@
+#motor controller
 
+while True:
+    try:
+        emp  = int(input("Enter Motor Speed: "))
+        print(f"Speed set to: {emp}")
+        break
+
+    except ValueError:
+        print("Error: Corrupted Signal. Maintaining current speed.")
+
+def get_coordinate():
+
+    while True:
+        coordinate = int(input("What is the X coordinate? "))
+
+        print(f"Coordinate X set to: {coordinate}")
+        break
+    
+    while False:
+    
+        coordinate = (input("What is the X coordinate? "))
+        print("Invalid coordinate")
+
+    get_coordinate()
